8be72375d0c80e642684ce827725cf44d03ec70d
Sandra Drąg <wisnianie@email.com>
2026-09-08 12:30:34 +0200

    tak test

--- projektTestowy/src/Main.java
+++ projektTestowy/src/Main.java
@@ -1,13 +1,19 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+    System.out.println("wiśnia");// to jest komentarz jednoliniowy i ogolnie "sout" skrot
+
+    /*
+    to wieloliniowy komentarz 1:1 jak w JS
+     */
+
+    //double jK Float ale 15 po przecinku tak
+
+    int calkowita=5;
+    double rzeczywista=4.4;
+
+    System.out.println("Wartość zmiennej \"całkowitej\" wynosi "  +calkowita);
+
+    System.out.println("Wartość zmiennej \"całkowita\" po podzieleniu na dwa: " + calkowita/2);
 }
