5da92d5d5ffc807e58fd769126d20b1ce0044490
Sandra Drąg <wisnia_nie@email.com>
2026-09-11 11:41:44 +0200

    zadanie 6 ze słowami

--- Zadania01/src/Main.java
+++ Zadania01/src/Main.java
@@ -2,19 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
 
-
+//zadanie 1
     System.out.println("Ania");
     System.out.println("Bartek");
     System.out.println("Kasia");
 
-
+//zadanie 2
     String imie = "Sandra";
     int rok_urodzenia = 2008;
     double liczba_przechow = 0.66;
 
+    //zadanie 3
     System.out.println("Mam na imię "+imie+ " ,mam "+(2026-rok_urodzenia) + " lat i będę pisać maturę za "+liczba_przechow+ " roku.");
 
-
+//zadanie 4
     System.out.println("Podaj jaką masz temperaturę na polu");
 
     Scanner sc = new Scanner(System.in);
@@ -23,8 +24,37 @@ void main() {
 
     double fahrenheit = 1.8*stopnie+32;
 
+    if (fahrenheit>=67) {
+        System.out.println("TO chyba nie żyjesz.");
+    }
+
     System.out.println("Temperatura w stopniach Fahrenheita: "+fahrenheit);
 
+//zadanie 5
+    System.out.println("Program liczy obwód trójkąta. Wprowadź długości boków.");
 
+    int pierwszy_bok=sc.nextInt();
+    int drugi_bok=sc.nextInt();
+    int trzeci_bok=sc.nextInt();
+
+    int obwod_trujkont=pierwszy_bok+drugi_bok+trzeci_bok;
+
+    System.out.println("Obwód trójkąta wynosi: "+obwod_trujkont);
+
+//zadanie 6
+    System.out.println("Program wczytuje w odwrotnej kolejności trzy słowa wprowadzone przez użytkownika. Wprowadź trzy słowa:");
+
+    System.out.println("Pierwsze słowo: ");
+    sc.nextLine();
+    String pierwsze_slowo=sc.nextLine();
+    System.out.println("Drugie słowo: ");
+    String drugie_slowo=sc.nextLine();
+    System.out.println("Trzecie słowo: ");
+    String trzecie_slowo=sc.nextLine();
+
+    System.out.println(trzecie_slowo+", "+drugie_slowo+", "+pierwsze_slowo);
+
+
+    
 
 }
