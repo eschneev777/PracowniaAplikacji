12331f2b3427281b59ab931328e453f044f51163
Sandra Drąg <wisnia_nie@email.com>
2026-09-11 10:57:11 +0200

    zadanie2

--- Zadania01/src/Main.java
+++ Zadania01/src/Main.java
@@ -1,13 +1,17 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+
+    System.out.println("Ania");
+    System.out.println("Bartek");
+    System.out.println("Kasia");
+
+
+    String imie = "Sandra";
+    int rok_urodzenia = 2008;
+    double liczba_przechow = 0.66;
+
+    System.out.println("Mam na imię "+imie+ " ,mam "+(2026-rok_urodzenia) + " lat i będę pisać maturę za "+liczba_przechow+ " roku.");
+
 }
