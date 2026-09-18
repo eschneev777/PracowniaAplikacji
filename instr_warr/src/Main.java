50917728497e84c7a7bbb5edc505d20324eaba56
Sandra Drąg <email@email.com>
2026-09-18 10:03:55 +0200

    instrukcje war

---
+++ instr_warr/src/Main.java
@@ -0,0 +1,137 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+
+//
+//    boolean warunek =true;
+//    System.out.println(warunek);
+//
+//    if (warunek)
+//    {
+//        System.out.println("Warunek prawdziwy");
+//
+//    } else {
+//        System.out.println("Warunek nieprawdziwy");
+//    }
+//
+//    int liczba1=5;
+//    int liczba2=10;
+//    if (liczba1>liczba2) {
+//        System.out.println(liczba1 + " jest większa niż "+ liczba2);
+//
+//    } else if (liczba1==liczba2) {
+//        System.out.println("Liczby są równe");
+//    } else {
+//        System.out.println(liczba2+ " jest większa niż "+ liczba1 );
+//    }
+//
+//
+//    if(liczba1 != liczba2) {
+//
+//        System.out.println("Liczby są różne");
+//    }
+//
+//
+//    String napis1="napis";
+//    String napis2="napis";
+//
+//    if(napis1.equals(napis2)) {
+//        System.out.println("Napisy są takie same");
+//    }
+//
+//Scanner sc = new Scanner(System.in);
+//
+//    System.out.println("Podaj liczbę");
+//    int liczba =sc.nextInt();
+//
+//    switch (liczba) {
+//        case 0:
+//            System.out.println("Zmienna ma wartość zero");
+//            break;
+//        case 5:
+//            System.out.println("Zmienna ma wartość 5");
+//            break;
+//        case 67:
+//            System.out.println("Jesteś chory");
+//            break;
+//        default:
+//            System.out.println("Nie wybrałeś jakiejś specjalnej liczby");
+//    }
+//
+//
+//boolean wyrazenie1 = true && false; // 1 i 0 -> 0
+//    System.out.println(wyrazenie1);
+//
+//    boolean wyrazenie2 = true || false; // 1 lub 0 -> 1
+//    System.out.println(wyrazenie2);
+//
+//
+
+    //Logowanie
+
+//    String login, haslo;
+//    login="user";
+//    haslo="passwd";
+//
+//    String loginPodany, hasloPodane;
+//    Scanner sc = new Scanner(System.in);
+//
+//    System.out.println("Podaj login:");
+//    loginPodany=sc.nextLine();
+//    System.out.println("Podaj hasło:");
+//    hasloPodane=sc.nextLine();
+//
+//    if(hasloPodane.equals(haslo) && loginPodany.equals(login)) {
+//
+//        System.out.println("Zalogowałeś się");
+//    } else if(loginPodany.equals(login) && !hasloPodane.equals(haslo)) {
+//        System.out.println("Złe hasło");
+//    } else {
+//        System.out.println("Zły login lub hasło");
+//    }
+
+//
+//    String poraRoku="Lato";
+//    String tlumaczenie=switch(poraRoku) {
+//        case "Wiosna":
+//            yield "Fru:hling"; // case "Wiosna" -> "Fru:hling":
+//        case "Lato":
+//            yield "Sommer";
+//        case "Jesień":
+//            yield "Herbst";
+//        case "Zima":
+//            yield "Winter";
+//        default:
+//            yield "Słowo nieznane";
+//
+//
+//
+//    };
+//    System.out.println(tlumaczenie);
+//
+//
+//String temperatura = switch (poraRoku) {
+//    case "Wiosna", "Lato" -> "piekło";
+//    case "Jesień", "Zima" -> "jest super";
+//    default -> "wartość nieznana";
+//
+//};
+//
+//    System.out.println(temperatura);
+//
+//    // operator warunkowy trójargumentowy (warunek) ? tak : nie
+//
+//    int liczba = 10;
+//    String wynik = (liczba>0) ? "Dodatnia" : "Ujemna lub 0";
+//    System.out.println(wynik);
+//
+//
+//
+//
+//
+//
+
+    
+
+
+}
