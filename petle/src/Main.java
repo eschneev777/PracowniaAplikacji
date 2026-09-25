642210ac6c8e513ccf4816d0181b8a47e0faaa39
Twoje Imię <email@email.com>
2026-09-25 11:25:29 +0200

    zadanie 8

--- petle/src/Main.java
+++ petle/src/Main.java
@@ -28,11 +28,11 @@ void main() {
 
     //ZADANIE 1
 
-    Scanner sc=new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
     System.out.println("Wprowadź liczbę całkowitą dodatnią, a program wyświetli wszystkie liczby nieparzyste od podanej liczby");
 
-    int calkowita=sc.nextInt();
-    for(int i=1;i<=calkowita; i+=2){
+    int calkowita = sc.nextInt();
+    for (int i = 1; i <= calkowita; i += 2) {
         System.out.println(i + " ");
     }
 
@@ -40,23 +40,23 @@ void main() {
 
     System.out.println("Wprowadź liczbę całkowitą dodatnią, a program wypisze wszytskiee potęgi liczby 2 do wskazanej, podanej tu liczby");
 
-    int n=sc.nextInt();
-    for(int i=1; i<=n; i*=2) {
-        System.out.println(i+" ");
+    int n = sc.nextInt();
+    for (int i = 1; i <= n; i *= 2) {
+        System.out.println(i + " ");
     }
 
 
     //ZADANIE 3
 
     System.out.println("Program wczytuje liczby dopóki nie zostanie podane 0. Wyświetla sumę wprowadzonych liczb.");
-int suma=0;
-int liczba;
+    int suma = 0;
+    int liczba;
 
-    do{
-         liczba= sc.nextInt();
-        suma+=liczba;
+    do {
+        liczba = sc.nextInt();
+        suma += liczba;
 
-    }while (liczba!=0);
+    } while (liczba != 0);
     System.out.println(suma);
 
 
@@ -64,53 +64,53 @@ int liczba;
 
     System.out.println("Progran pobiera ciąg liczb całkowitych. Zakończenie pobierania ciągu liczb, kończy wpisanie zera. Na końcu program wyświetla sumę największej i najmniejszej liczby z ciągu oraz ich średnią arytmetyczną.");
 
-    int liczby=sc.nextInt();
+    int liczby = sc.nextInt();
 
-    int min=liczby;
-    int max=liczby;
+    int min = liczby;
+    int max = liczby;
 
-    while(true) {
-        liczby=sc.nextInt();
+    while (true) {
+        liczby = sc.nextInt();
 
-        if (liczby==0) {
+        if (liczby == 0) {
             break;
         }
 
-        if (liczby<min){
-            min=liczby;
+        if (liczby < min) {
+            min = liczby;
 
         }
 
-        if (liczby>max) {
-            max=liczby;
+        if (liczby > max) {
+            max = liczby;
         }
     }
 
-    int suma2=max+min;
-    double srednia=suma/2;
+    int suma2 = max + min;
+    double srednia = suma / 2;
 
-    System.out.println("Największa liczba: "+max);
-    System.out.println("Najmniejsza liczba: "+min);
-    System.out.println("Suma największej i najmniejszej: "+suma2);
-    System.out.println("Średnia największej i najmniejszej: "+srednia);
+    System.out.println("Największa liczba: " + max);
+    System.out.println("Najmniejsza liczba: " + min);
+    System.out.println("Suma największej i najmniejszej: " + suma2);
+    System.out.println("Średnia największej i najmniejszej: " + srednia);
 
 
 //ZADANIE 5
 
     System.out.println("Program losuje liczbę z zakresu 1 do 100, a użytkownik ma za zadanie odgadnąć jaka to liczba.");
 
-    Random random=new Random();
-    int wylosowana= random.nextInt(100)+1;
-    int wpisana=0;
+    Random random = new Random();
+    int wylosowana = random.nextInt(100) + 1;
+    int wpisana = 0;
 
-    while (wpisana!=wylosowana) {
+    while (wpisana != wylosowana) {
 
         if (sc.hasNextInt()) {
-            wpisana=sc.nextInt();
+            wpisana = sc.nextInt();
 
-            if(wpisana>wylosowana) {
+            if (wpisana > wylosowana) {
                 System.out.println("Za dużo");
-            } else if(wpisana<wylosowana) {
+            } else if (wpisana < wylosowana) {
                 System.out.println("za mało");
             } else {
                 System.out.println("Gratulacje użytkowniku, odgadłeś liczbę");
@@ -120,6 +120,60 @@ int liczba;
 
 
     }
+
+//ZADANIE 6 - nie wiem
+
+    System.out.println("Program tworzy prostokąt ze znaków.");
+
+
+    System.out.println("Podaj długość boku A (szerokość)");
+    int a = sc.nextInt();
+
+    System.out.println("Podaj długość boku B (długość)");
+    int b = sc.nextInt();
+
+    System.out.println("Wprowadź współrzędną X rogu");
+    int x = sc.nextInt();
+
+    System.out.println("Wprowadź współrzędną Y rogu");
+    int y = sc.nextInt();
+
+    System.out.println("Wprowadź znak wypełnienia");
+    String zn1=sc.nextLine();
+    String zn=sc.nextLine();
+
+
+    for (int i = 1; i < y; i++) {
+        System.out.println(" ");
+    }
+
+    for (int i = 0; i < b; i++) {
+
+        for (int j = 1; j < x; j++) {
+            System.out.println("  ");
+        }
+
+
+        for (int j = 0; j < a; j++) {
+            System.out.println(zn);
+        }
+
+        System.out.println(" ");
+    }
+
+
+//ZADANIE 7 nie wiem
+
+//ZADANIE 8
+
+    System.out.println("Program liczy silnie. Wprowadź liczbę z której ma zostać policzona.");
+    int wpr=sc.nextInt();
+    int silnia=1;
+
+    for (int i=1; i<=wpr; i++) {
+        silnia*=i;
+    }
+    System.out.println("Silnia z " +wpr+ " wynosi "+silnia);
 
 
 
