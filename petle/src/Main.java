1342d17aa1862fc153d86d412b4c971fd2df062e
Twoje Imię <email@email.com>
2026-09-25 12:00:28 +0200

    zadanie 10

--- petle/src/Main.java
+++ petle/src/Main.java
@@ -191,6 +191,23 @@ void main() {
         System.out.println("słowo NIE jest palindromem");
     }
 
+//ZADANIE 10
 
+    petla:
+    for(int i=1; i<=10; i++) {
+        if (i%2!=0) {
+            continue;
+        }
+
+        for (int j=1; j<=10; j++) {
+            System.out.println(j+" ");
+
+            if (j>i) {
+                System.out.println("skok żaby");
+                continue petla;
+            }
+        }
+        System.out.println();
+    }
 
 }
